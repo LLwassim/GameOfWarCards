098e1f5084dc76c4fb94ee1e82493eaaf1e756e0
Wassim Lkorchy <lacorchy77@gmail.com>
2019-11-25 18:17:15 -0500

    done

--- script.js
+++ script.js
@@ -1,8 +1,218 @@
-function multiply(num1, num2) {
-  return num2 * num1
-  console.log(num3)
+let aUsersturns = [];
+let aDealerturns = [];
+
+let userscore = 26;
+let dealerscore = 26;
+
+function lotocards() {
+
+  var usercard = Math.floor(Math.random() * 13 + 2);
+  aUsersturns.push(usercard);
+  console.log(aUsersturns);
+
+  var usercardoutput = "";
+  if (usercard === 11) {
+    usercardoutput = "J"
+  } else {
+    if (usercard === 12) {
+      usercardoutput = "Q";
+    } else {
+      if (usercard === 13) {
+        usercardoutput = "K";
+      } else {
+        if (usercard === 14) {
+          usercardoutput = "A";
+        } else {
+          usercardoutput = usercard;
+        }
+      }
+    }
+  }
+
+  document.getElementById("usercardnum").innerHTML = usercardoutput;
+
+  var dealercard = Math.floor(Math.random() * 13 + 2);
+  aDealerturns.push(dealercard);
+  console.log(aDealerturns);
+
+  var dealercardoutput = "";
+  if (dealercard === 11) {
+    dealercardoutput = "J"
+  } else {
+    if (dealercard === 12) {
+      dealercardoutput = "Q";
+    } else {
+      if (dealercard === 13) {
+        dealercardoutput = "K";
+      } else {
+        if (dealercard === 14) {
+          dealercardoutput = "A";
+        } else {
+          dealercardoutput = dealercard;
+        }
+      }
+    }
+  }
+
+  document.getElementById("dealercardnum").innerHTML = dealercardoutput;
+
+  // Dealer Chars:
+  var dealerchar = Math.floor(Math.random() * 4 + 1);
+  console.log("dealerchar " + dealerchar);
+
+  switch (dealerchar) {
+    case 1:
+      dealerchar = "&hearts;";
+      document.getElementById("dealerchar").style.color = "red";
+      document.getElementById("dealerbigchar").style.color = "red";
+      break;
+
+    case 2:
+      dealerchar = "&spades;";
+      document.getElementById("dealerchar").style.color = "black";
+      document.getElementById("dealerbigchar").style.color = "black";
+      break;
+
+    case 3:
+      dealerchar = "&diams;";
+      document.getElementById("dealerchar").style.color = "red";
+      document.getElementById("dealerbigchar").style.color = "red";
+      break;
+
+    case 4:
+      dealerchar = "&clubs;";
+      document.getElementById("dealerchar").style.color = "black";
+      document.getElementById("dealerbigchar").style.color = "black";
+      break;
+  }
+  document.getElementById("dealerchar").innerHTML = dealerchar;
+  document.getElementById("dealerbigchar").innerHTML = dealerchar;
+
+  // User Chars:
+  var userchar = Math.floor(Math.random() * 4 + 1);
+  console.log("userchar " + userchar);
+
+  switch (userchar) {
+    case 1:
+      userchar = "&hearts;";
+      document.getElementById("userchar").style.color = "red";
+      document.getElementById("userbigchar").style.color = "red";
+      break;
+
+    case 2:
+      userchar = "&spades;";
+      document.getElementById("userchar").style.color = "black";
+      document.getElementById("userbigchar").style.color = "black";
+      break;
+
+    case 3:
+      userchar = "&diams;";
+      document.getElementById("userchar").style.color = "red";
+      document.getElementById("userbigchar").style.color = "red";
+      break;
+
+    case 4:
+      userchar = "&clubs;";
+      document.getElementById("userchar").style.color = "black";
+      document.getElementById("userbigchar").style.color = "black";
+      break;
+  }
+  document.getElementById("userchar").innerHTML = userchar;
+  document.getElementById("userbigchar").innerHTML = userchar;
+
+
+  // Scores:
+  if (usercard === dealercard) {
+    ifqual();
+    document.getElementById("pointer").innerHTML = "=";
+  } else {
+    if (usercard > dealercard) {
+      userscore++;
+      dealerscore--;
+      document.getElementById("pointer").innerHTML = "►";
+    } else {
+      if (dealercard > usercard) {
+        dealerscore++;
+        userscore--;
+        document.getElementById("pointer").innerHTML = "◄";
+      }
+    }
+  }
+
+  if (userscore < 10) {
+    document.getElementById("userscore").style.color = "red";
+  } else {
+    document.getElementById("userscore").style.color = "black";
+  }
+
+  if (dealerscore < 10) {
+    document.getElementById("dealerscore").style.color = "red";
+  } else {
+    document.getElementById("dealerscore").style.color = "black";
+  }
+
+  // Display Scores:
+  document.getElementById("dealerscore").innerHTML = dealerscore;
+  document.getElementById("userscore").innerHTML = userscore;
+
+  console.log("user score: " + userscore);
+  console.log("dealer score: " + dealerscore);
+
+  // Display Turns:
+  var dealerturns = "";
+  var i;
+  for (i = 0; i < aDealerturns.length; i++) {
+    dealerturns = dealerturns + aDealerturns[i] + ", ";
+    document.getElementById("dealerturns").innerHTML = dealerturns
+  }
+
+  var userturns = "";
+  var j;
+  for (j = 0; j < aUsersturns.length; j++) {
+    userturns = userturns + aUsersturns[j] + ", ";
+    document.getElementById("userturns").innerHTML = userturns
+  }
+
+  chekingscores();
 }
 
-multiply(2,3);
+function ifqual() {
+  var usercard = Math.floor(Math.random() * 13 + 2);
+  aUsersturns.push(usercard);
+  console.log(aUsersturns);
 
-console.log("test")
+  var dealercard = Math.floor(Math.random() * 13 + 2);
+  aDealerturns.push(dealercard);
+  console.log(aDealerturns);
+
+  if (usercard === dealercard) {
+    ifqual();
+    document.getElementById("pointer").innerHTML = "=";
+  } else {
+    if (usercard > dealercard) {
+      userscore = userscore + 4;
+      dealerscore = dealerscore - 4;
+      document.getElementById("pointer").innerHTML = "►";
+    } else {
+      if (dealerscore > userscore) {
+        dealerscore = dealerscore + 4;
+        userscore = userscore - 4;
+        document.getElementById("pointer").innerHTML = "◄";
+      }
+    }
+  }
+}
+
+function chekingscores() {
+  if (userscore <= 0) {
+    userscore = 0;
+    alert("Dealer Win!");
+    document.getElementById("mainbtn").disabled = true;
+  } else {
+    if (dealerscore <= 0) {
+      userscore = 0;
+      alert("User Win!");
+      document.getElementById("mainbtn").disabled = true;
+    }
+  }
+}
